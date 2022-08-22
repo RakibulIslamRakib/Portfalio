2a0384d4d21892c194cee178a4641cdcdcce6602
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-22 17:26:37 +0600

    all utility added

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfalio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult projects()
+        {
+            return View();
+        }
+
+        public IActionResult projectsDetails()
+        {
+            return View();
+        }
+    }
+}
